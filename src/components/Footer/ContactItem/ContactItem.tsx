511f3eaaabf61fcb82406805f0290691cb52bbe9
Valentin Panov <valentin.panov@me.com>
2021-11-02 22:23:54 +0300

    feat(footer) design done

--- src/components/Footer/ContactItem/ContactItem.tsx
+++ src/components/Footer/ContactItem/ContactItem.tsx
@@ -11,13 +11,22 @@ export type Props = {
   connectText: string;
 };
 
-export const ContactItem = memo<Props>(({ className, connectIconAlt, connectLink, connectIconSrc, connectText }) => (
-  <li>
-    <Link to={connectLink} className={cn(s.root, className)}>
-      <div className={s.icon__container}>
-        <img src={connectIconSrc} alt={connectIconAlt} className={s.icon__image} />
-      </div>
-      <span className={s.connection__text}>{connectText}</span>
-    </Link>
-  </li>
-));
+export const ContactItem = memo<Props>(({ className, connectIconAlt, connectLink, connectIconSrc, connectText }) => {
+  const textArr = connectText.split('<br>');
+  return (
+    <li>
+      <Link to={connectLink} className={cn(s.root, className)}>
+        <div className={s.icon__container}>
+          <img src={connectIconSrc} alt={connectIconAlt} className={s.icon__image} />
+        </div>
+        <div className={s.text__container}>
+          {textArr.map((el) => (
+            <span className={s.connection__text} key={el}>
+              {el}
+            </span>
+          ))}
+        </div>
+      </Link>
+    </li>
+  );
+});
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,6 +15,8 @@ import gPlus from './img/gPlus.svg';
 import twt from './img/twt.svg';
 import linkedIn from './img/linkedIn.svg';
 import btnUp from './img/btnUp.svg';
+import { FooterTitle } from './FooterTitle';
+import { FooterSubtitle } from './FooterSubtitle';
 
 export type Props = {
   className?: string;
@@ -24,9 +26,7 @@ export const Footer = memo<Props>(({ className }) => (
   <footer className={cn(s.root, className)}>
     <div className={s.top}>
       <section className={s.topSection}>
-        <div className={s.connectBlockTitle}>
-          <span className={s.footer__title}>Свяжитесь с нами</span>
-        </div>
+        <FooterTitle text="Свяжитесь с нами" />
         <ul className={s.connectBlockContainer}>
           <ContactItem
             connectLink="/"
@@ -50,38 +50,44 @@ export const Footer = memo<Props>(({ className }) => (
             connectLink="/"
             connectIconSrc={iconGeo}
             connectIconAlt="iconGeo"
-            connectText="г. Москва ул. Московская 27-35 555 555"
+            connectText="г. Москва<br>ул. Московская 27-35<br>555 555"
           />
         </ul>
       </section>
       <section className={s.topSection}>
-        <div className={s.connectBlockTitle}>
-          <span>Подписка</span>
-        </div>
-        <div className={s.connectBlockSubtitle}>
-          <p>Будьте в курсе событий</p>
-          <Form className={s.subscriptionForm}>
-            <Input placeholder="e-mail" className={s.emailInput} />
-            <Button className={s.btn}>ОТПРАВИТЬ</Button>
-          </Form>
-        </div>
-        <div className={s.socials}>
-          <span>Подписывайтесь на нас</span>
-          <ul className={s.socials__imageContainer}>
-            <SocialIcon iconSrc={youtube} iconLink="/" iconAlt="youtube" />
-            <SocialIcon iconSrc={linkedIn} iconLink="/" iconAlt="linkedIn" />
-            <SocialIcon iconSrc={gPlus} iconLink="/" iconAlt="gPlus" />
-            <SocialIcon iconSrc={fb} iconLink="/" iconAlt="fb" />
-            <SocialIcon iconSrc={twt} iconLink="/" iconAlt="twt" />
-          </ul>
-        </div>
+        <FooterTitle text="Подписка" />
+        <FooterSubtitle text="Будьте в курсе событий" />
+        <Form className={s.subscriptionForm}>
+          <Input placeholder="e-mail" className={s.emailInput} />
+          <Button className={s.btn} htmlType="submit">
+            ОТПРАВИТЬ
+          </Button>
+        </Form>
+        <FooterTitle text="Подписывайтесь на нас" />
+        <ul className={s.socials__imageContainer}>
+          <SocialIcon iconSrc={youtube} iconLink="/" iconAlt="youtube" />
+          <SocialIcon iconSrc={linkedIn} iconLink="/" iconAlt="linkedIn" />
+          <SocialIcon iconSrc={gPlus} iconLink="/" iconAlt="gPlus" />
+          <SocialIcon iconSrc={fb} iconLink="/" iconAlt="fb" />
+          <SocialIcon iconSrc={twt} iconLink="/" iconAlt="twt" />
+        </ul>
       </section>
     </div>
-    <div className={cn(s.bottom)}>
+    <div className={s.bottom}>
       <Logo />
-      <img src={btnUp} alt="up" />
-      <span>2018 Web</span>
+      <Button
+        shape="circle"
+        className={s.upBtn}
+        onClick={() =>
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          })
+        }
+      >
+        <img src={btnUp} alt={btnUp} />
+      </Button>
+      <span className={s.date}>2018 Web</span>
     </div>
-    ;
   </footer>
 ));
